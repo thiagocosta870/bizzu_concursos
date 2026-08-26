eb7fb46977c5dabced914fc5d430345fdb8c88e3
thiagocosta870 <titocosta416@gmail.com>
2026-08-26 19:18:32 -0300

    Funcionalidade de cadastro de concursos com o analytics

--- lib/controllers/cadastro_concurso_controller.dart
+++ lib/controllers/cadastro_concurso_controller.dart
@@ -1,5 +1,7 @@
 import 'package:bizzu_concursos/models/concurso_model.dart';
 import 'package:bizzu_concursos/models/repositories/concurso_repository.dart';
+import 'package:firebase_analytics/firebase_analytics.dart';
+import 'package:flutter/foundation.dart';
 
 class CadastroConcursoController {
   final ConcursoRepository _repository = ConcursoRepository();
@@ -10,8 +12,8 @@ class CadastroConcursoController {
     required String dataProva,
     required String materias,
     required String usuarioId,
-  }) {
-    return _repository.salvarConcurso(
+  }) async {
+    bool sucesso = await _repository.salvarConcurso(
       ConcursoModel(
         nome: nome,
         cargo: cargo,
@@ -20,6 +22,17 @@ class CadastroConcursoController {
       ),
       usuarioId,
     );
+
+    if (sucesso) {
+      try {
+        await FirebaseAnalytics.instance.logEvent(name: 'cadastrar_concurso');
+        debugPrint('ANALYTICS: Concurso cadastrado com sucesso!');
+      } catch (e) {
+        debugPrint('ANALYTICS ERRO: $e');
+      }
+    }
+
+    return sucesso;
   }
 
   Future<bool> atualizarConcurso({
@@ -29,8 +42,8 @@ class CadastroConcursoController {
     required String dataProva,
     required String materias,
     required String usuarioId,
-  }) {
-    return _repository.atualizarConcurso(
+  }) async {
+    bool sucesso = await _repository.atualizarConcurso(
       ConcursoModel(
         id: id,
         nome: nome,
@@ -40,5 +53,16 @@ class CadastroConcursoController {
       ),
       usuarioId,
     );
+
+    if (sucesso) {
+      try {
+        await FirebaseAnalytics.instance.logEvent(name: 'editar_concurso');
+        debugPrint('ANALYTICS: Edição de concurso registrada!');
+      } catch (e) {
+        debugPrint('ANALYTICS ERRO: $e');
+      }
+    }
+
+    return sucesso;
   }
 }
